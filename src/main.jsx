62348af50d648a687762476ccbb764cd514d64ec
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-05-07 00:29:33 +0600

    Header & Footer Completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 
-import { createBrowserRouter, RouterProvider } from 'react-router';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello World</div>,
-  },
-]);
+import { RouterProvider } from 'react-router';
+import router from './Routs/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </StrictMode>
 );
