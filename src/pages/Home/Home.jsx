ea7370338bfca435c6c30055479b2b4fdadaecf2
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-05-07 00:48:19 +0600

    Added Data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Banner from '../../Components/Banner/Banner';
+import Books from '../Books/Books';
+// import { data } from 'react-router';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch('booksData.json')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <Banner />
+      <Books />
     </div>
   );
 };
